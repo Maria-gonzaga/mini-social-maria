912823a32da71cc8ebf52a97c1dad8ec29927552
Maria-gonzaga <maria.9553712@aluno.mg.gov.br>
2026-05-06 14:16:43 +0000

    Up date atualizar tela

--- js/script.js
+++ js/script.js
@@ -5,23 +5,22 @@ let curtido = false;
 let descurtido = false;
 
 //=== SERVICE(regras de negócio)===
+
 function curtir() {
   if (curtido == false){
     likeCount++;
     curtido = true;
-    document.getElementById("likeCount").innerText = likeCount;
-
+    
     if(descurtido == true){
       dislikeCount--;
       descurtido = false;
-      document.getElementById("dislikeCount").innerText = dislikeCount;
-
+     
     }
 
   }else{
     likeCount--;
     curtido = false;
-    document.getElementById("likeCount").innerText = likeCount;
+   
   }
 
 }
@@ -30,32 +29,47 @@ function descurtir() {
   if(descurtido == false){
     dislikeCount++;
     descurtido = true;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
+   
 
     if(curtido == true){
       likeCount--;
       curtido = false;
-      document.getElementById("likeCount").innerText = likeCount;
+     
     }
 
   }
   else{
     dislikeCount--;
     descurtido = false;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
+    
   }
+}
+// ===VIEW (interface/renderização)===
+function atualizarTela(){
+   document.getElementById("dislikeCount").innerText = dislikeCount;
+document.getElementById("dislikeCount").innerText = dislikeCount;
+
+
+    }
+
 }
 
 //=== CONTROLLER (intermediação)===
 
 function clicarcurtir(){
    curtir();
+   atualizarTela();
 }
 function clicarDescurtir(){
    descurtir();
+   atualizarTela();
 }
 
 // === EVENTOS ===
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
 document.getElementById("dislikeBtn").addEventListener("click", descurtir);
+ 
+// === INICIALIZAÇÃO ===
+
+atualizarTela
